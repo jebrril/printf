98829a95c45715e90ce63c89e8bbf8428bb5b72f
jebrril <125573348+jebrril@users.noreply.github.com>
2023-04-25 15:49:54 +0000

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -4,7 +4,7 @@
 
 /**
  * print_char - Prints a char
- * by KHEIREDDINE & NABALSSI
+ * by JEBRRIL
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
